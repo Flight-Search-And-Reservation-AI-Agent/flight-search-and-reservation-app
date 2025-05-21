81bfa8a412e270e2575280a58f57fa707cffea95
Crusher95574 <97830371+Crusher95574@users.noreply.github.com>
2025-05-21 20:02:07 +0530

    ChecklistService unit testing done

--- src/main/java/com/mycompany/flightapp/dto/CheckListDTO.java
+++ src/main/java/com/mycompany/flightapp/dto/CheckListDTO.java
@@ -1,8 +1,12 @@
 package com.mycompany.flightapp.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CheckListDTO {
     private String task;
 
